624706b5f6ec421feb256f8c0c98808b80730500
Sarran Wrap <christian.sarran@mail.utoronto.ca>
2018-12-14 15:30:23 -0500

    a

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,8 +4,8 @@ public class App
 {
     public static void main( String[] args )
     {
-       System.out.println( "Hello World!" );
-	System.out.println( "Hello World!NN" );
+       	System.out.println( "Hello World!" );
+	System.out.println( "Hello World!" );
 	System.out.println( "Hello World!" );
 	System.out.println( "Hello World!" );
 	System.out.println( "Hello World!" );
